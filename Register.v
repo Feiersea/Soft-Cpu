ef20b2ed2c9ea9cf188787a919ba50944b6c7c11
Sean Feiertag <113747121+Feiersea@users.noreply.github.com>
2025-05-04 19:05:02 -0500

    Update Register.v

--- Register.v
+++ Register.v
@@ -1,7 +1,14 @@
 module Register (
-    input wire enable;
-    input wire clk;
-    input wire reg_in1;
-    output reg reg_out1;
-
+    input wire enable,
+    input wire reset,
+    input wire clk,
+    input wire reg_in1,
+    output reg reg_out1
 );
+    always @ (posedge clk or negedge reset) begin
+        if (!reset)
+            reg_out1 <= 1'b0;
+        else if (enable)
+            reg_out1 <= reg_in1;
+    end
+endmodule
